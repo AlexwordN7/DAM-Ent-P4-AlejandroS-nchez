34c6d050977e7be65a388aa7ba114b1808364d13
alexs <alexs@LAPTOP-KQGHEVBO>
2020-03-06 19:33:47 +0100

    ladosIguales

--- src/main/java/liceosorollab/Rectangulo.java
+++ src/main/java/liceosorollab/Rectangulo.java
@@ -1,5 +1,34 @@
 package main.java.liceosorollab;
 
 public class Rectangulo {
-
+	private int lado1;
+	private int lado2;
+	
+	public Rectangulo(int lado1, int lado2) {
+		this.lado1=4;
+		this.lado2=4;
+	}
+	
+	public int getlado1() {
+		return this.lado1;
+	}
+	public void setlado1() {
+		this.lado1=lado1;
+	}
+	
+	public int getlado2() {
+		return this.lado2;
+	}
+	public void setlado2() {
+		this.lado2=lado2;
+	}
+	
+	public boolean comprobarLadosIguales() {
+		boolean iguales=false;
+		 if (lado1==lado2) {
+			iguales=true;
+		 }
+		return iguales;
+	}
+	
 }
--- src/test/java/liceosorollab/RectanguloTest.java
+++ src/test/java/liceosorollab/RectanguloTest.java
@@ -1,5 +1,20 @@
 package test.java.liceosorollab;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorollab.Rectangulo;
 
 public class RectanguloTest {
-
+	private Rectangulo rectangulo;
+	
+	@Before
+	public void Before() {
+		rectangulo=new Rectangulo(6,9);
+	}
+	
+	@Test
+	public void comprobarLadosIguales() {
+		assertEquals(false,rectangulo.comprobarLadosIguales());
+	}
 }
