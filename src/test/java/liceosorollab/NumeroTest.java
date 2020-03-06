9a51fc3e7c0649d2e05f84bb4e1d306fb2251b16
DAM_1 <DAM_1@PB05>
2020-03-06 20:33:15 +0100

    numero1

--- src/test/java/liceosorollab/NumeroTest.java
+++ src/test/java/liceosorollab/NumeroTest.java
@@ -1,5 +1,25 @@
 package test.java.liceosorollab;
 
-public class NumeroTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorollab.Numero;
+
+
+public class NumeroTest {
+	
+	private Numero numero;
+
+	
+	@Before
+	public void Before() {
+		numero=new Numero(1,5);
+	}
+	
+	@Test
+	public void comprobarnumero1() {
+		assertEquals(1,numero.getNumero1());
+	}
 }
